bf50529704f947ac275c128d953318628a45d316
n-kall <n-kall@github.com>
2023-02-15 10:47:59 +0200

    remove print statement

--- R/moment_match.R
+++ R/moment_match.R
@@ -119,7 +119,6 @@ moment_match.matrix <- function(x,
     lw <- as.vector(weights(lw_psis))
     k <- lw_psis$diagnostics$pareto_k
     if (any(is.infinite(k))) {
-      print(exp(lw))
       stop("Something went wrong, and encountered infinite Pareto k values..")
     }
 
